4a01e68012f61764c741e22b9df6ec445ceb382f
The64thRealm <36716817+The64thRealm@users.noreply.github.com>
2025-12-01 17:24:44 -0800

    added some functionality to database accessor, started login request servlet

--- src/main/java/DatabaseAccessor.java
+++ src/main/java/DatabaseAccessor.java
@@ -1,8 +1,6 @@
 
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DatabaseAccessor {
 
@@ -11,10 +9,17 @@ public class DatabaseAccessor {
     private static final String JDBC_PASS = "root";
 
     private static Connection connection = null;
+    private static PreparedStatement getUserFromUsernameStatement = null;
+    private static PreparedStatement getUserFromEmailStatement = null;
+    private static PreparedStatement insertUserStatement = null;
 
     static {
         try {
             connection = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASS);
+
+            getUserFromUsernameStatement = connection.prepareStatement("SELECT * FROM users WHERE username = ?");
+            getUserFromEmailStatement = connection.prepareStatement("SELECT * FROM users WHERE email = ?");
+            insertUserStatement = connection.prepareStatement("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
         } catch (SQLException e) {
             e.printStackTrace();
             throw new RuntimeException("Could not initialize database connection, please check server: " + e.getMessage());
@@ -27,5 +32,54 @@ public class DatabaseAccessor {
         } else {
             throw new SQLException("Could not initialize database connection, please check server");
         }
+    }
+
+    public static boolean LoginUser(String username, String password) {
+        if (username == null || password == null) { return false; }
+
+        try {
+            getUserFromUsernameStatement.setString(1, username);
+            ResultSet rs = getUserFromUsernameStatement.executeQuery();
+            if (rs.next() && rs.getString("password").equals(password)) {
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    public static boolean CheckUserExists(String username) {
+        try {
+            getUserFromEmailStatement.setString(1, username);
+            ResultSet rs = getUserFromUsernameStatement.executeQuery();
+            return rs.next();
+        } catch (SQLException e) {}
+        return false;
+    }
+
+    public static boolean CheckEmailExists(String email) {
+        try {
+            getUserFromEmailStatement.setString(1, email);
+            ResultSet rs = getUserFromEmailStatement.executeQuery();
+            return rs.next();
+        } catch (SQLException e) {}
+        return false;
+    }
+
+    public static boolean RegisterUser(String username, String password, String email) {
+        if (CheckUserExists(username) || CheckEmailExists(email)) return false;
+
+        try {
+            insertUserStatement.setString(1, username);
+            insertUserStatement.setString(2, email);
+            insertUserStatement.setString(3, password);
+            insertUserStatement.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
---
+++ src/main/java/LoginRequestServlet.java
@@ -0,0 +1,31 @@
+package main.java;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+@WebServlet("/LoginRequestServlet")
+public class LoginRequestServlet extends HttpServlet {
+    @Override
+    protected void doPost(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        if (DatabaseAccessor.LoginUser(username, password)) {
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.setContentType("application/json");
+
+            // TODO write json response
+        } else {
+            resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+    }
+}
